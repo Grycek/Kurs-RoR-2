3e9684800161c72d40463bd55d6ed229708ac056
Grycek <adam.grycner@wp.pl>
2010-10-15 12:42:37 +0200

    Generate application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Task2::Application.config.secret_token = 'a28fa2bb8e6926bed115c30a7bcdbf873df31af084ec64d67fcf0f2b8c63951ca4d6027f507ca9e1cc59411e9073f925a7c57314a72b3e3210afac050e914ece'
